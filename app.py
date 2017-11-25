aee592545b6e2687c917789395d35b8ad0a7d505
devsdmf <devsdmf@gmail.com>
2017-11-25 16:33:31 -0200

    added after request middleware and changed default entrypoint to return a json response

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 
-from flask import Flask
+from flask import Flask, json
 
+# initializing flask framework
 app = Flask(__name__)
 
+# setting up after_request middleware
+@app.after_request
+def apply_default_headers(res):
+    res.headers['Content-Type'] = 'application/json'
+    res.headers['Server'] = 'CaaS.Server'
+    return res
+
+# added default entrypoint
 @app.route('/')
 def index():
-    return "Hello, CaaS!"
+    return json.jsonify({"hello": "caas"})
 
+# health check endpoint
 @app.route('/health',methods=['GET'])
 def health_check():
     return ''
 
+# app run
 if __name__ == '__main__':
     app.run(debug=True)
